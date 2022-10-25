e2f79c222c8f1964d257104b7f981d9514be0af5
DarkillWaoW <darkyll@orange.fr>
2022-10-25 16:25:33 +0200

    All Project
    All project initialite push

---
+++ src/Controller/TestController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+
+class TestController
+{
+
+    public function index()
+    {
+        dd("route index");
+    }
+
+    /**
+     * @Route("/test/{age<\d+>?0}", name="test", methods={"GET", "POST"}, host="localhost")
+     */
+
+    public function test(Request $request, $age)
+    {
+        var_dump($age);
+        // $request = Request::createFromGlobals();
+
+        // dump($request);
+        // echo "\n";
+
+        //$age = $request->query->get('age', 0);
+
+        // $age = $request->attributes->get('age');
+
+
+
+        return new Response("j'ai $age ans");
+    }
+}
