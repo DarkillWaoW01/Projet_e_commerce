93bc985fa99c384bbd55d3417b6c67fe3e4f73b7
DarkillWaoW <darkyll@orange.fr>
2022-12-12 15:21:03 +0100

    Add variable Calculator initiate in Services.yaml
    Le container de services essaye d'injecter la variable float $tva automatiquement mais ne la connait pas. il va donc chercher l'info dans le fichier config/services.yaml

--- src/ServiceTest/Calculator.php
+++ src/ServiceTest/Calculator.php
@@ -8,17 +8,20 @@ class Calculator
 {
 
     protected $logger;
+    protected $tva;
 
-    public function __construct(LoggerInterface $logger)
+    public function __construct(LoggerInterface $logger, float $tva)
     {
 
         $this->logger = $logger;
+        $this->tva = $tva;
     }
 
     public function calcul(float $prix): float
     {
 
         $this->logger->info("un caclul a lieu");
+        dump($this->tva);
         return $prix * 20 / 100;
     }
 }
