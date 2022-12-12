13bcf8ddbe72ea6300bb694e64eb576a3da58ad9
DarkillWaoW <darkyll@orange.fr>
2022-12-12 15:48:13 +0100

    Add Slugify to hello controller
    Ajout direct de slugify dans le container de service (services.yaml) pour qu'il puisse être instancier par le container de services dans le controller hello

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\ServiceTest\Calculator;
+use Cocur\Slugify\Slugify;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,25 +12,18 @@ use Symfony\Component\Routing\Annotation\Route;
 class HelloController
 {
 
-    protected $calculator;
-
-    public function __construct(Calculator $calculator)
-    {
-        $this->calculator = $calculator;
-    }
-
     /**
      * @Route("/hello/{nom<[a-zA-Z]+>?World}", name="hello")
      */
 
     // on aurait aussi pu mettre "/hello/{nom<[a-zA-Z]+>}", name="hello", defaults={"nom":"World"} )
 
-    public function hello(Request $request, $nom, LoggerInterface $logger): Response
+    public function hello(Request $request, $nom, LoggerInterface $logger, Calculator $calculator, Slugify $slugify): Response
     {
 
         $logger->error("coucou");
 
-        dump($this->calculator->calcul(100));
+        dump($calculator->calcul(100));
 
         return new Response("Hello $nom");
     }
